6f0490b0e1edeb20aaa227a53e9a7a0090ec89d8
Peter Kraft <petereliaskraft@gmail.com>
2025-03-12 16:24:37 -0700

    concurrency

--- temporal-application/temporal/workflows.py
+++ temporal-application/temporal/workflows.py
@@ -1,3 +1,5 @@
+import asyncio
+import logging
 from datetime import timedelta
 from typing import List
 
@@ -7,14 +9,22 @@ from temporalio import workflow
 with workflow.unsafe.imports_passed_through():
     from temporal.activities import IndexDocumentsParams, index_document
 
+logger = logging.getLogger("temporal-worker")
+
 
 @workflow.defn(name="IndexDocumentsWorkflow")
 class IndexDocumentsWorkflow:
     @workflow.run
     async def run(self, urls: List[str]):
+        futures = []
         for url in urls:
-            await workflow.execute_activity(
+            future = workflow.execute_activity(
                 index_document,
                 IndexDocumentsParams(url),
                 start_to_close_timeout=timedelta(seconds=60),
             )
+            futures.append(future)
+        results = await asyncio.gather(*futures)
+        indexed_pages = sum(results)
+        logger.info(f"Indexed {len(urls)} documents totaling {indexed_pages} pages")
+        return indexed_pages
